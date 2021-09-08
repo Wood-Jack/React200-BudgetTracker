2d751d2226fe6ab02e6624b30b1fe4e68c47db9e
Wood-Jack <thewoodrowz@gmail.com>
2021-09-08 14:52:41 -0400

    Completed Budget Tracker Up and running

---
+++ src/js/components/ExpenseEntries/expensesReducer.js
@@ -0,0 +1,53 @@
+
+const defaultState ={
+    description:'',
+    amount: '',
+    lineItems:[]
+};
+
+export default function ExpenseReducer (state = defaultState, action){
+
+    const { type, payload } = action;
+
+    switch(type){
+
+        case `UPDATE_EXPENSE_DESCRIPTION`:{
+
+            return{
+
+                ...state,
+
+                description: payload.description
+
+            };
+        }
+
+        case `UPDATE_EXPENSE_AMOUNT` :{
+
+            return{
+
+                ...state,
+                amount: payload.amount
+            }
+        }
+
+        case `ADD_EXPENSE` : {
+
+            const {desciption, amount} = action.payload;
+
+            return{
+                
+                description: '',
+                action: '',
+                lineItems: [
+                    ...state.lineItems,
+                    {description, amount}
+                ]
+            };
+        }
+
+        default: {
+            return state;
+        }
+    }
+}
---
+++ src/js/components/ExpenseEntries/index.js
@@ -0,0 +1,16 @@
+import ExpenseEntries from './ExpenseEntries';
+import {connect} from 'react-redux';
+
+function mapStoreToProps(store){
+
+    return{
+        description: store.expense.description,
+        amount: store.expense.amount,
+        lineItems: store.expense.lineItems
+    };
+}
+
+
+
+
+export default connect(mapStoreToProps)(ExpenseEntries);
